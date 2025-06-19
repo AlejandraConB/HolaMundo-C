5cad846eeb65cbc11c158f25222b6e64437eab2c
Alejandra Contreras <user.email>
2025-06-18 22:48:31 -0400

    Primer commit

---
+++ HolaMundo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>;
+int main() {
+printf("Hello World\n");
+return 0;
+}
+
+/*Basic Types (Tipos de Datos):
+Aritmeticos
+(A) Valores enteros:
+
+char 1 byte
+unsigned char 1 byte
+
+int 4 bytes
+unsigned int 4 bytes no permite números negativos
+
+short 2 bytes
+unsigned short 2 bytes
+
+long 8 bytes
+unsigned long 8 bytes
+
+(B) De punto flotante
+float 4 bytes 6 decimal places
+double 8 bytes 15 decimal places
+long double 10 byes 19 decimal places
+*/
